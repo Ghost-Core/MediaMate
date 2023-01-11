ebc57c850be317f286b7cf83becdb309d027dd07
Amin Ahmadi <71956668+Ghost-Core@users.noreply.github.com>
2023-01-11 11:23:49 -0500

    MediaMate

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import {
+  ChannelDetail,
+  VideoDetail,
+  SearchFeed,
+  Navbar,
+  Feed,
+} from './components';
+
+const App = () => (
+  <HashRouter>
+    <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </HashRouter>
+);
+
+export default App;
